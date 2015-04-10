c3a1a545ad49b6f0ce03ef9a9cdd0ed511e39115
Kyle Gerard Felker <kfelker@princeton.edu>
2015-04-10 15:34:53 -0400

    Reorganized matrices as dynamically allocated contiguous arrays

--- main_matmul.c
+++ main_matmul.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "mpi.h"
 #include <time.h>
 
-#define NRA 10
-#define NCA 10
-#define NCB 7
 #define MASTER 0
 #define FROM_MASTER 1
 #define FROM_WORKER 2
@@ -20,9 +18,9 @@ int initializeMatrix(double **M, int NR,int NC){
   return(0);
 }
 
-void main(int argc, char **argv){
+int main(int argc, char **argv){
   int nprocs, myid, nworkers, source, dest, mtype, rows, averow, extra, offset, i,j, k, rc;
-
+  int NRA,NCA,NCB;
   MPI_Status status;
 
   MPI_Init(&argc,&argv);
@@ -37,12 +35,34 @@ void main(int argc, char **argv){
     MPI_Abort(MPI_COMM_WORLD, rc);
     exit(1);
   }
+  /* Dynamically allocate matrices */
+    NRA = atoi(argv[1]);
+    NCA = atoi(argv[2]);
+    NCB = atoi(argv[3]);
 
   /*Master task*/
   if (myid == MASTER){
-    double A[NRA][NCA], B[NCA][NCB], C[NRA][NCB];
+    double **A,**B,**C; 
+    double *dataA,*dataB,*dataC;
+    //make contiguous multiarrays
+    A = (double **) malloc(sizeof(double *)*NRA); 
+    dataA = (double *) malloc(sizeof(double)*NRA*NCA); 
+    B = (double **) malloc(sizeof(double *)*NCA); 
+    dataB = (double *) malloc(sizeof(double)*NCA*NCB); 
+    C = (double **) malloc(sizeof(double *)*NRA); 
+    dataC = (double *) malloc(sizeof(double)*NRA*NCB); 
+    for(int i=0; i<NRA; i++){
+      A[i] = (double *) &(dataA[NCA*i]);
+    }  
+    for(int i=0; i<NCA; i++){
+      B[i] = (double *) &(dataB[NCB*i]);
+    }  
+    for(int i=0; i<NRA; i++){
+      C[i] = (double *) &(dataC[NCB*i]);
+    }  
     initializeMatrix(A, NRA, NCA);
     initializeMatrix(B, NCA, NCB);
+    printf("here i am\n");
     MPI_Bcast(B, NCA*NCB, MPI_DOUBLE, 0, MPI_COMM_WORLD); //Broadcast
 
     averow = NRA/nworkers;
@@ -57,29 +77,40 @@ void main(int argc, char **argv){
       offset += rows;
     }
     MPI_Waitall(3*nworkers, ireq, &stat);
-    mtype = FROM_WORKER
+    mtype = FROM_WORKER;
     for(source = 1; source < nprocs; ++dest){
       MPI_Recv(&offset, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &stat);
       MPI_Recv(&rows, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &stat);
       MPI_Recv(&C[offset][0], rows*NCA, MPI_DOUBLE, source, mtype, MPI_COMM_WORLD, &stat);
     }
 
-    free(ireq);
+    //free(ireq);
   }else{ //Worker process
-    double B[NCA][NCB];
+    double **B; 
+    double *dataB;
+    //make contiguous multiarrays
+    B = (double **) malloc(sizeof(double *)*NCA); 
+    dataB = (double *) malloc(sizeof(double)*NCA*NCB); 
+    for(int i=0; i<NCA; i++){
+      B[i] = (double *) &(dataB[NCB*i]);
+    }  
+
     MPI_Bcast(B, NCA*NCB, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     //Synchronous receive (?)
     mtype = FROM_MASTER;
+
     MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &stat);
     MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &stat);
     double **A = malloc(rows*sizeof(double *));
+    double *dataA = (double *) malloc(sizeof(double)*NCA*rows); 
     for(i = 0; i<rows; ++i){
-      A[i] = malloc(NCA*sizeof(double));
+      A[i] = &(dataA[NCA*i]);
     }
     MPI_Recv(&A, rows*NCA, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &stat);
     double **C = malloc(rows*sizeof(double *));
+    double *dataC = (double *) malloc(sizeof(double)*NCB*rows); 
     for(i = 0; i<rows; ++i){
-      C[i] = malloc(NCB*sizeof(double));
+      C[i] = &(dataC[NCB*i]);
     }
     for(k = 0; k<NCB; ++k){
       for(i = 0; i<rows; ++i){
@@ -93,15 +124,15 @@ void main(int argc, char **argv){
     MPI_Send(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
     MPI_Send(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
     MPI_Send(&C, rows*NCB, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD);
-    for(i = 0; i<rows; ++i){
+    /*for(i = 0; i<rows; ++i){
       free(C[i]);
       free(A[i]);
     }
     free(C);
-    free(A);
+    free(A); */
   }  
   printf("hello world\n"); 
   MPI_Finalize();
-  return;
+  return(0);
 
 }
