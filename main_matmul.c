2798f7451a212229293b4848340d302141d80aac
Kyle Gerard Felker <kfelker@princeton.edu>
2015-04-16 21:19:06 -0700

    debugging matmul. there is a strange minimum error where the value of NCB and rows is reset on the worker process.... run with mpirun -np 2 ./a.out 10 10 10 to see

--- main_matmul.c
+++ main_matmul.c
@@ -19,8 +19,9 @@ int initializeMatrix(double **M, int NR,int NC){
 }
 
 int main(int argc, char **argv){
-  int nprocs, myid, nworkers, source, dest, mtype, rows, averow, extra, offset, i,j, k, rc;
+  int nprocs, myid, nworkers, source, dest, mtype, averow, extra, offset, i,j, k, rc;
   int NRA,NCA,NCB;
+  int rows; 
   MPI_Status status;
 
   MPI_Init(&argc,&argv);
@@ -67,19 +68,24 @@ int main(int argc, char **argv){
     extra = NRA%nworkers;
     offset = 0;
     mtype = FROM_MASTER;
+    printf("master before sends \n");
+    printf("%d %d\n",NCB,rows);
     for(dest = 1; dest<nprocs; ++dest){
-      rows = (dest <= extra) ? averow+1 : averow;
+      rows = (dest <= extra) ? averow+1 : averow;      
       MPI_Isend(&offset, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD,ireq + dest-1);
       MPI_Isend(&rows, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD, ireq + nworkers + dest-1);
       MPI_Isend(&A[offset][0], rows*NCA, MPI_DOUBLE, dest, mtype, MPI_COMM_WORLD, ireq + 2*nworkers + dest - 1);
       offset += rows;
     }
-    MPI_Waitall(3*nworkers, ireq, &stat);
+    printf("master after sends \n");
+    MPI_Waitall(3*nworkers, ireq, &stat); //kyle: do we need to wait for the messages to complete before posting the recieves?
     mtype = FROM_WORKER;
     for(source = 1; source < nprocs; ++dest){
       MPI_Recv(&offset, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &stat);
       MPI_Recv(&rows, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &stat);
-      MPI_Recv(&C[offset][0], rows*NCA, MPI_DOUBLE, source, mtype, MPI_COMM_WORLD, &stat);
+      //kyle edit: shouldnt this be rows*NCB?
+      //      MPI_Recv(&C[offset][0], rows*NCA, MPI_DOUBLE, source, mtype, MPI_COMM_WORLD, &stat);
+      MPI_Recv(&C[offset][0], rows*NCB, MPI_DOUBLE, source, mtype, MPI_COMM_WORLD, &stat);
     }
 
     //free(ireq);
@@ -96,17 +102,24 @@ int main(int argc, char **argv){
     MPI_Bcast(B, NCA*NCB, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     //Synchronous receive (?)
     mtype = FROM_MASTER;
-
+    printf("worker before recvs \n");
     MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &stat);
     MPI_Recv(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &stat);
-    double **A = malloc(rows*sizeof(double *));
+    printf("worker NCB: %d rows: %d\n",NCB,rows);
+    double **A = (double **) malloc(rows*sizeof(double *));
     double *dataA = (double *) malloc(sizeof(double)*NCA*rows); 
     for(i = 0; i<rows; ++i){
       A[i] = &(dataA[NCA*i]);
     }
+    printf("worker before recv A \n");
     MPI_Recv(&A, rows*NCA, MPI_DOUBLE, MASTER, mtype, MPI_COMM_WORLD, &stat);
-    double **C = malloc(rows*sizeof(double *));
+    printf("worker after recv A\n");
+    double **C = (double **) malloc(rows*sizeof(double *));
+    printf("worker after allocate C **\n");
+    printf("worker NCB: %d rows: %d\n",NCB,rows);
+    //KYLE: FOR SOME REASON, NCB AND ROWS GETS RESET FROM 10 LINES UP TO NOW
     double *dataC = (double *) malloc(sizeof(double)*NCB*rows); 
+    printf("worker after allocate C \n");
     for(i = 0; i<rows; ++i){
       C[i] = &(dataC[NCB*i]);
     }
@@ -118,6 +131,7 @@ int main(int argc, char **argv){
 	}
       }
     }
+    printf("worker after recvs \n");
     mtype = FROM_WORKER;
     MPI_Send(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
     MPI_Send(&rows, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
